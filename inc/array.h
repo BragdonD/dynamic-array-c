224bb61c6ec2a173ec0dcb893e249d36d08220a0
Thomas DUCLOS <thomasduclos1012@gmail.com>
2022-10-21 22:31:25 +0900

    add test check and documentation

--- inc/array.h
+++ inc/array.h
@@ -9,17 +9,26 @@ typedef struct array {
     size_t length;
 } array;
 
+/// Allocation functions
+
 array* arr_init(size_t data_size);
+array* linkedList_create_from_list(void* arr, int len, size_t type_size);
+void arr_free(array *arr);
+
+/// Accessor functions
+
 size_t arr_lenght(array* arr);
+void* arr_front(array* arr);
+void* arr_end(array* arr);
+bool arr_is_empty(array* arr);
+
+/// Operation functions
+
 bool arr_push_back(array* arr, void* elem);
 void* arr_pop_back(array* arr);
 bool arr_insert(array* arr, int index, void* elem);
 void* arr_erase(array* arr, int index);
 void arr_swapp(array* arr1, array* arr2);
 void arr_empty(array* arr);
-void* arr_front(array* arr);
-void* arr_end(array* arr);
-bool arr_is_empty(array* arr);
-array* linkedList_create_from_list(void* arr, int len, size_t type_size);
 
 #endif
